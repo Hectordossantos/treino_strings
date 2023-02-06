bd5f7639b30928cfc853e9093d729655b6e46b8a
Héctor dos Santos <124292120+Hectordossantos@users.noreply.github.com>
2023-02-06 15:01:54 -0300

    Create imprimir_primeiras_quatro_letras.c

---
+++ imprimir_primeiras_quatro_letras.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+char string[50];
+char string2[6];
+
+printf("digite seu nome \n");
+scanf("%s", &string);
+
+   int cont;
+    for(cont = 0; cont < 4; cont ++){   
+        string2[cont] = string[cont];
+    }
+    printf("nome %s \n", string2);
+  
+    return 0;
+}
